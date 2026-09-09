5fe0e7b9bc1e5bd7d3237e1d9273288913943666
EdenDvorkind <tchstud2026e3701.github@gmail.com>
2026-09-09 17:05:39 +0300

    feat(many): map typeORM entities according to DB

---
+++ backend/src/Entities/Drone.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { DronesGroup } from "./DronesGroup.entity";
+import { DroneType } from "./DroneType.entity";
+
+@Entity("drone")
+export class Drone {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ name: "drones_group_id" })
+  dronesGroupId!: number;
+
+  @Column({ type: "float" })
+  longitude!: number;
+
+  @Column({ type: "float" })
+  latitude!: number;
+
+  @Column({ type: "float" })
+  asl!: number;
+
+  @Column({ type: "float" })
+  agl!: number;
+
+  @Column({ type: "float" })
+  heading!: number;
+
+  @Column({ type: "float" })
+  velocity!: number;
+
+  @Column()
+  type!: number;
+
+  @ManyToOne(
+    () => DronesGroup,
+    (dronesGroup: DronesGroup) => dronesGroup.drones,
+  )
+  @JoinColumn({ name: "drones_group_id" })
+  dronesGroup!: DronesGroup;
+
+  @ManyToOne(() => DroneType, (droneType: DroneType) => droneType.drones)
+  @JoinColumn({ name: "type" })
+  droneType!: DroneType;
+}
