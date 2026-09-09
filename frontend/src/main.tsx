2c9e6ce6f7896d40be5e7146f160695b73b3e3bf
Yoni Blecher Narbaev <u9755937@bsmch.net>
2026-09-09 16:02:47 +0300

    feat(main.tsx): added providors to app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { CssBaseline } from '@mui/material';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CssBaseline } from "@mui/material";
+import App from "./App";
+import { AttackerProvider, DefenseProvider } from "./contexts";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <AttackerProvider>
+      <DefenseProvider>
+        <App />
+      </DefenseProvider>
+    </AttackerProvider>
   </React.StrictMode>,
 );
