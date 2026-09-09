0df3e40a845bb0700834c8ff6cefe7314ba4e338
Eldor Patouri <u9741209@bsmch.net>
2026-09-09 16:12:45 +0300

    feat(Many) create simulation context

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
 import App from './App';
+import { SimulationProvider } from './simulation/SimulationProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <CssBaseline />
-    <App />
+    <SimulationProvider>
+      <App />
+    </SimulationProvider>
   </React.StrictMode>,
 );
