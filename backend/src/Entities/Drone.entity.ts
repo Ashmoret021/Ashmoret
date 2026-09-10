e266be9f8244471b904a02ff44c0cd56e96bcd7e
notshnits <jonatan03042002@gmail.com>
2026-09-10 17:29:28 +0300

    added start time field

--- backend/src/Entities/Drone.entity.ts
+++ backend/src/Entities/Drone.entity.ts
@@ -34,6 +34,9 @@ export class Drone {
   @Column({ type: "float" })
   velocity!: number;
 
+  @Column({ type: "float", name: "start_time" })
+  startTime!: number;
+
   @Column({ type: "int" })
   type!: number;
 
